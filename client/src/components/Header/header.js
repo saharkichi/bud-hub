6d757ffca5be251fb4d626746c3ff7f96db4575e
linklg1 <105123660+linklg1@users.noreply.github.com>
2022-07-30 14:11:16 -0700

    reformatwithfewestreacterrors

---
+++ client/src/components/Header/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../Navbar/Navbar';
+
+function Header() {
+    return (
+        <div className="header">
+            <h1>Bud Hub</h1>
+            <Navbar />
+        </div>
+    )
+}
+
+export default Header
