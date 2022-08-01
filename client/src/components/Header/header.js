58c74ec0d7f96d7937d27636b64353f003f323a7
Mariya Setko <mariya.setko@gmail.com>
2022-07-31 20:28:56 -0400

    added logo to header and made it a link

--- client/src/components/Header/header.js
+++ client/src/components/Header/header.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Navbar from '../Navbar/Navbar';
 import { NavLink } from "react-router-dom";
+import Logo from "../../assets/images/BudHub-logo.png"
 import "./style.css";
 
 function Header() {
     return (
         <div className="header">
-           <NavLink className={({ isActive }) => (isActive ? 'active' : 'inactive')}to="/Home"> <h1>Bud Hub</h1> </NavLink>
+           <NavLink className={({ isActive }) => (isActive ? 'active' : 'inactive')}to="/Home"> <img className="logo" src={Logo} alt="BudHub"></img> </NavLink>
             <NavLink className={({ isActive }) => (isActive ? 'active' : 'inactive')}to="/Login">Login </NavLink>        
             <Navbar />    
         </div>
