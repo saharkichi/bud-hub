6d55f5e6f8f6e6d8dfc2ba143b2c6d28c6903310
saharkichi <saharkichi@gmail.com>
2022-08-02 10:17:46 -0400

    updated problems with AboutUs.js file

--- client/src/components/Login/LoginPage.js
+++ client/src/components/Login/LoginPage.js
@@ -1,15 +1,12 @@
 // import Loginform from './components/Login/LoginForm';
 import React from 'react';
 import "./style.css";
-import LoginForm from './components/Login/LoginForm';
 
 function LoginPage() {
     return (
      <div>Login
          Signup
-         <div>
-            
-         </div>
+        
      </div>
     )
 }
